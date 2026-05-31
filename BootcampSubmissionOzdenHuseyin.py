c182c11670340c486a9a6002023898de53ffda6b
ozdenhuseyin <ohus0003@student.monash.edu>
2026-06-01 00:01:16 +1000

    Fixed bug where only highest number was returned rather than most counted.

--- BootcampSubmissionOzdenHuseyin.py
+++ BootcampSubmissionOzdenHuseyin.py
@@ -5,7 +5,7 @@
 #3. Get max from dictionary by looping through each number in the dictionary
 
 #List to search through
-newList = [1,1,2,3,44,4,4,4,4]
+newList = [1,1, 1, 1, 1, 1,2,3,44,4,4,4,4]
 
 #Empty dictionary (to store key : value pair)
 myDic = {}
@@ -21,11 +21,14 @@ for num in newList:
         myDic[num] = 1
 
 #Cycle through each number in the dictionairy.
-#If the number's "count" is larger than res, assign res to larger number.
-res = 0
+#If the number's "count" is larger than max_count, assign res to higher reccuring number,
+#and max_count to the corresponding value.
+res = None
+max_count = 0
 for num in myDic:
-    if myDic[num] > res:
+    if myDic[num] > max_count:
         res = num
+        max_count = myDic[num]
 
 #printing solution
 print(res)
